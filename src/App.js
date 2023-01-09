af442b02f051290855b51b011c512dabe6e91f27
Rohit kumar <rohitkumarsinghmuz54@gmail.com>
2023-01-09 22:15:26 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useMemo, useState } from "react";
 
-function App() {
+const App = () => {
+  const [count, setCount] = useState(0);
+  const [decrement, setDecrement] = useState(200);
+  const incrementData = () => {
+    setCount(count + 1);
+    console.log("incremented value");
+  };
+  const Decrementdata = () => {
+    setDecrement(decrement - 1);
+    console.log("Decrement value");
+  };
+  // const multiply = () => {
+  //   console.log("multiply  function");
+  //   return count * 10;
+  // };
+
+  const multiplyfunction = useMemo(() => {
+    console.log("multiply  function");
+    return count * 10;
+  }, [count]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      {multiplyfunction}
+      {/* {multiply()} */}
+      <div style={{ width: "70%", margin: "15px auto" }}>
+        <h2>count{count}</h2>
+        <button
+          style={{ padding: "10px 20px", margin: "10px" }}
+          onClick={incrementData}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          Incrment
+        </button>
+
+        <button
+          style={{ padding: "10px 20px", margin: "10px" }}
+          onClick={Decrementdata}
+        >
+          Decrement
+        </button>
+        <h2>decrement {decrement}</h2>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
